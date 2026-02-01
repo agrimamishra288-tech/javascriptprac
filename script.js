c8449db849fe9557000530df167b4b3f9127174c
Agrima Mishra <agrimamishra288@gmail.com>
2026-02-01 23:58:26 +0530

    web dev : getElementById and element children

---
+++ script.js
@@ -0,0 +1,44 @@
+// debuggergit 
+// console.log(a)
+//console.log(b)
+
+// var a= 78 //part of global scope
+// let b=345 //
+
+// console.log(a)
+// console.log(b)
+
+
+// function print(){
+//     let c=45
+//     console.log(c)
+//     console.log("Inside fn")
+// }
+// print()
+
+// function calclulate(){
+//     console.log(total)
+
+    // console.log("First line")
+    // setTimeout(()=>(
+        // console.log("After 2 seconds")),2000)
+        // console.log("second line")
+
+        // setTimeout(()=>(
+        //     alert("Alert after 3 seconds)),3*1000)")
+//         // ))
+// let v=1
+//     const timerId=setInterval(()=>{
+//         console.log(v);
+//         v++
+//     },1000)
+// //console.logtimerid
+//         setTimeout(()=>{
+//             clearInterval(timerId)
+//         },10*1000)
+
+
+const container=document.getElementById("container");
+
+container.firstElementChild.style.background="red";
+container.lastElementChild.style.background="green";
---
+++ web1getelement.js
@@ -0,0 +1,4 @@
+const container = document.getElementById("container");
+
+container.firstElementChild.style.background="red";
+container.lastElementChild.style.background="green";
